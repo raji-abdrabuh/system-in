0a01e2b03b470480398270a13df03178fdd67e6f
raji-abdrabuh <raji200022@gmail.com>
2025-10-21 19:39:43 +0300

    2

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,17 @@
-import type { NextConfig } from "next";
+// import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
+// const nextConfig: NextConfig = {
+//   /* config options here */
+// };
 
-export default nextConfig;
+// export default nextConfig;
+
+const nextConfig = {
+  output: 'export',
+  trailingSlash: true,
+  images: {
+    unoptimized: true
+  }
+}
+
+module.exports = nextConfig
